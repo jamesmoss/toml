799dda7d6ef9e64a1a985bc262c4db5eb57503ac
James Moss <james@message.co.uk>
2013-02-24 21:57:51 +0000

    The parameters in the `assertEquals` were the wrong way round. Derp.

--- tests/Toml/ParserTest.php
+++ tests/Toml/ParserTest.php
@@ -7,52 +7,58 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 	public function testParsingStrings()
 	{
 		$p = Parser::fromString('title = "TOML example"');
-		$this->assertEquals($p, array('title' => 'TOML example'));
+		$this->assertEquals(array('title' => 'TOML example'), $p);
 	}
 
 	public function testParsingStringsWithLineBreaks()
 	{
 		$p = Parser::fromString('bio = "PHP Developer\nLives in Brighton, England."');
-		$this->assertEquals($p, array('bio' => "PHP Developer\nLives in Brighton, England."));
+		$this->assertEquals(array('bio' => "PHP Developer\nLives in Brighton, England."), $p);
 	}
 
 	public function testParsingIntegers()
 	{
 		$p = Parser::fromString('age = 27');
-		$this->assertEquals($p, array('age' => 27));
+		$this->assertEquals(array('age' => 27), $p);
 	}
 
 	public function testParsingFloats()
 	{
 		$p = Parser::fromString('pi = 3.14');
-		$this->assertEquals($p, array('pi' => 3.14));
+		$this->assertEquals(array('pi' => 3.14), $p);
 	}
 
 	public function testParsingDates()
 	{
 		$p = Parser::fromString('dob = 1985-10-10T07:00:00Z');
-		$this->assertEquals($p, array('dob' => new \Datetime('1985-10-10T07:00:00Z')));
+		$this->assertEquals(array('dob' => new \Datetime('1985-10-10T07:00:00Z')), $p);
 	}
 
 	public function testParsingBoolean()
 	{
 		$p = Parser::fromString('enabled = true');
-		$this->assertEquals($p, array('enabled' => true));
+		$this->assertEquals(array('enabled' => true), $p);
 
 		$p = Parser::fromString('enabled = false');
-		$this->assertEquals($p, array('enabled' => false));
+		$this->assertEquals(array('enabled' => false), $p);
 	}
 
 	public function testParsingArray()
 	{
 		$p = Parser::fromString('ports = [9001, 9002, 9003]');
-		$this->assertEquals($p, array('ports' => array(9001, 9002, 9003)));
+		$this->assertEquals(array('ports' => array(9001, 9002, 9003)), $p);
 	}
 
 	public function testParsingMultiArray()
 	{
 		$p = Parser::fromString('data = [ [ 1, 2 ], ["a", "b" , "c" ] ]');
-		$this->assertEquals($p, array('data' => array(array(1, 2), array('a', 'b', 'c'))));
+		$this->assertEquals(array('data' => array(array(1, 2), array('a', 'b', 'c'))), $p);
+	}
+
+	public function testParsingMultiArrayWithTrailingCommas()
+	{
+		$p = Parser::fromString('data = [ [ 1, 2, ], ["a", "b" , "c", ], ]');
+		$this->assertEquals(array('data' => array(array(1, 2), array('a', 'b', 'c'))), $p);
 	}
 
 	/**
@@ -66,24 +72,24 @@ class ParserTest extends \PHPUnit_Framework_TestCase
 	public function testParsingComments()
 	{
 		$p = Parser::fromString("# This is a comment\ntitle=\"TOML Example\"");
-		$this->assertEquals($p, array('title' => 'TOML Example'));
+		$this->assertEquals(array('title' => 'TOML Example'), $p);
 	}
 
 	public function testLoadingFromFile()
 	{
 		$p = Parser::fromFile(__DIR__.'/example.toml');
-		$this->assertEquals($p['title'], 'TOML Example');
+		$this->assertEquals('TOML Example', $p['title']);
 	}
 
 	public function testKeyGroup()
 	{
 		$p = Parser::fromString("[main]\nip = \"192.168.1.1\"");
-		$this->assertEquals($p, array('main' => array('ip' => '192.168.1.1')));
+		$this->assertEquals(array('main' => array('ip' => '192.168.1.1')), $p);
 	}
 
 	public function testNestedKeyGroup()
 	{
 		$p = Parser::fromString("[main.beta]\nip = \"192.168.1.1\"");
-		$this->assertEquals($p, array('main' => array('beta' => array('ip' => '192.168.1.1'))));
+		$this->assertEquals(array('main' => array('beta' => array('ip' => '192.168.1.1'))), $p);
 	}
 }
